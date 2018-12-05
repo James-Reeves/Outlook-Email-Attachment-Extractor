978d90c86fdd036124eb4497dfd29c38c73506d7
James Reeves <James.Reeves@TheInquisitiveAnalyst.com>
2018-12-05 09:54:13 -0600

    Initial files

---
+++ OutlookEmailAttachmentGrabber/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace OutlookEmailAttachmentGrabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(600, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Exit Application";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SlateGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(713, 37);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Email Attachment Extractor";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.SlateGray;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(133, 39);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(448, 45);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "This application connects to your outlook email, iterates through your inbox fold" +
+    "er and sub-folders, and then saves your text attachments.";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(129, 83);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(456, 221);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TabStop = false;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(231, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 52);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Load Email Handle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(360, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Save .txt Attachments";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.SlateGray;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(231, 310);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(123, 19);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Select a Value";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Black;
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Run",
+            "Quit"});
+            this.comboBox1.Location = new System.Drawing.Point(361, 310);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 27);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(715, 475);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Email Attachment Grabber";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ OutlookEmailAttachmentGrabber/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookEmailAttachmentGrabber
+{
+    public partial class Form1 : Form
+    {
+
+        static string basePath = @"C:\Emails\";
+        static int totalfilesize = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.SetOut(new ControlWriter(textBox3));
+            MessageBox.Show("Click the \"Load Email\" button to load your email handle into the program.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EnumerateAccounts();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExtractAttachments();
+        }
+
+        public class ControlWriter : TextWriter
+        {
+            private Control textbox;
+            public ControlWriter(Control textbox)
+            {
+                this.textbox = textbox;
+            }
+
+            public override void Write(char value)
+            {
+                textbox.Text += value;
+            }
+
+            public override void Write(string value)
+            {
+                textbox.Text += value;
+            }
+
+            public override Encoding Encoding
+            {
+                get { return Encoding.ASCII; }
+            }
+        }
+
+        public static void EnumerateFoldersInDefaultStore()
+        {
+            Outlook.Application Application = new Outlook.Application();
+            Outlook.Folder root = Application.Session.DefaultStore.GetRootFolder() as Outlook.Folder;
+            EnumerateFolders(root);
+        }
+
+        // Uses recursion to enumerate Outlook subfolders.
+        public static void EnumerateFolders(Outlook.Folder folder)
+        {
+            Outlook.Folders childFolders = folder.Folders;
+            if (childFolders.Count > 0)
+            {
+                foreach (Outlook.Folder childFolder in childFolders)
+                {
+                    // We only want Inbox folders - ignore Contacts and others
+                    if (childFolder.FolderPath.Contains("Inbox"))
+                    {
+                        // Write the folder path.
+                        Console.WriteLine(childFolder.FolderPath);
+                        // Call EnumerateFolders using childFolder, to see if there are any sub-folders within this one
+                        EnumerateFolders(childFolder);
+                    }
+                }
+            }
+            Console.WriteLine("Checking in " + folder.FolderPath);
+            IterateMessages(folder);
+        }
+
+        public static void IterateMessages(Outlook.Folder folder)
+        {
+            // attachment extensions to save. Since this is only for my HIN return files I will limit this to .txt only
+            string[] extensionsArray = { ".txt" };
+
+            // Iterate through all items ("messages") in a folder
+            var fi = folder.Items;
+            if (fi != null)
+            {
+
+                try
+                {
+                    foreach (Object item in fi)
+                    {
+                        Outlook.MailItem mi = (Outlook.MailItem)item;
+                        var attachments = mi.Attachments;
+                        if (attachments.Count != 0)
+                        {
+
+                            // Create a directory to store the attachment 
+                            if (!Directory.Exists(basePath + folder.FolderPath))
+                            {
+                                Directory.CreateDirectory(basePath + folder.FolderPath);
+                            }
+
+                            //Console.WriteLine(mi.Sender.Address);
+                            //Console.WriteLine(mi.Subject + " [" + attachments.Count + "]");
+                            //Console.WriteLine(generateFolder(folder.FolderPath, mi.Sender.Address));
+                            for (int i = 1; i <= mi.Attachments.Count; i++)
+                            {
+                                var fn = mi.Attachments[i].FileName.ToLower();
+                                //check wither any of the strings in the extensionsArray are contained within the filename
+                                if (extensionsArray.Any(fn.Contains))
+                                {
+
+                                    // Create a further sub-folder for the sender
+                                    if (!Directory.Exists(basePath + folder.FolderPath + @"\" + mi.Sender.Address))
+                                    {
+                                        Directory.CreateDirectory(basePath + folder.FolderPath + @"\" + mi.Sender.Address);
+                                    }
+                                    totalfilesize = totalfilesize + mi.Attachments[i].Size;
+                                    if (!File.Exists(basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName))
+                                    {
+                                        Console.WriteLine("Saving " + mi.Attachments[i].FileName);
+                                        mi.Attachments[i].SaveAsFile(basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName);
+                                        //mi.Attachments[i].Delete();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Already saved " + mi.Attachments[i].FileName);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine("An error occurred: '{0}'", e);
+                }
+            }
+        }
+
+        // Retrieves the email address for a given account object
+        public static string EnumerateAccountEmailAddress(Outlook.Account account)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(account.SmtpAddress) || string.IsNullOrEmpty(account.UserName))
+                {
+                    Outlook.AddressEntry oAE = account.CurrentUser.AddressEntry as Outlook.AddressEntry;
+                    if (oAE.Type == "EX")
+                    {
+                        Outlook.ExchangeUser oEU = oAE.GetExchangeUser() as Outlook.ExchangeUser;
+                        return oEU.PrimarySmtpAddress;
+                    }
+                    else
+                    {
+                        return oAE.Address;
+                    }
+                }
+                else
+                {
+                    return account.SmtpAddress;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        static void EnumerateAccounts()
+        {
+            Console.WriteLine("Outlook Attachment Extraction Tool");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("The Inquisitive Analyst");
+            Console.WriteLine("Created by: James Reeves");
+            Console.WriteLine();
+            int id;
+            Outlook.Application Application = new Outlook.Application();
+            Outlook.Accounts accounts = Application.Session.Accounts;
+
+            id = 1;
+            foreach (Outlook.Account account in accounts)
+            {
+                Console.WriteLine("Run: " + EnumerateAccountEmailAddress(account));
+                id++;
+            }
+            Console.WriteLine("Quit: Quit Application");
+            Console.WriteLine();
+            Console.WriteLine("Select Run or Quit from the provided box, then click the \"Save .txt Attachments\" button.");
+        }
+
+        public void ExtractAttachments()
+        {
+            string response = "";
+            Outlook.Application Application = new Outlook.Application();
+            Outlook.Accounts accounts = Application.Session.Accounts;
+
+            response = UsersInput();
+
+            if (response == "Q")
+            {
+                Console.WriteLine("Quitting...");
+                Form1.ActiveForm.Close();
+            }
+            if (response != "")
+            {
+            
+                    Console.WriteLine("Processing: " + accounts[Int32.Parse(response.Trim())].DisplayName);
+                    Console.WriteLine("Processing: " + EnumerateAccountEmailAddress(accounts[Int32.Parse(response.Trim())]));
+
+                    Outlook.Folder selectedFolder = Application.Session.DefaultStore.GetRootFolder() as Outlook.Folder;
+                    selectedFolder = GetFolder(@"\\" + accounts[Int32.Parse(response)].DisplayName);
+                    EnumerateFolders(selectedFolder);
+                    MessageBox.Show("Job Successfully Completed!" + "\n" + "Your files have been saved here:" +
+                        "\n" + @"C:\Emails\");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Account Selected");
+            }
+            
+        }
+
+        public string UsersInput()
+        {
+            string userInput = "";
+
+            if (comboBox1.Text == "Quit")
+            {
+                userInput = "Q";
+            }
+            if (comboBox1.Text == "Run")
+            {
+                userInput = "1";
+            }
+            return userInput;
+        }
+
+        // Returns Folder object based on folder path
+        static Outlook.Folder GetFolder(string folderPath)
+        {
+            //MessageBox.Show("Looking for: " + folderPath);
+            Outlook.Folder folder;
+            string backslash = @"\";
+            try
+            {
+                if (folderPath.StartsWith(@"\\"))
+                {
+                    folderPath = folderPath.Remove(0, 2);
+                }
+                String[] folders = folderPath.Split(backslash.ToCharArray());
+                Outlook.Application Application = new Outlook.Application();
+                folder = Application.Session.Folders[folders[0]] as Outlook.Folder;
+                if (folder != null)
+                {
+                    for (int i = 1; i <= folders.GetUpperBound(0); i++)
+                    {
+                        Outlook.Folders subFolders = folder.Folders;
+                        folder = subFolders[folders[i]] as Outlook.Folder;
+                        if (folder == null)
+                        {
+                            return null;
+                        }
+                    }
+                }
+                return folder;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
